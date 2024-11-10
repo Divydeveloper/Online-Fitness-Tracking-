836e0c1cdf1f54470752b45b2f47fd0997d50ddd
Divydeveloper <gaurdivyansh2005@gmail.com>
2024-11-10 21:32:11 +0530

    Initial commit

--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/fitness_tracker";
     private static final String USER = "root";
-    private static final String PASSWORD = "ayush2004";
+    private static final String PASSWORD = "divyansh@123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
